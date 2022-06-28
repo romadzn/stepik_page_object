93050177867d03166e0dd0099d8fdad2cf9baf64
roman romanov <romanoff280@yandex.ru>
2022-06-28 13:40:24 +0500

    add transition to login page

--- test_main_page.py
+++ test_main_page.py
@@ -3,33 +3,19 @@ from selenium.webdriver.common.by import By
 from pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                    # открываем страницу
-    page.go_to_login_page          # выполняем метод страницы — переходим на страницу логина (passed)
-
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()     # переходим на страницу с логином
+    login_page = LoginPage(browser, browser.current_url)    # инициализируем LoginPage
+    login_page.should_be_login_page() # делаем проверки login_page
+    
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/" 
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()    # проверяем, есть ли на странице ссылка на логин (passed)
     
-###  
+
   
-def test_login_should_be_in_url(browser):   
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_page()    # проверяем содержит ли url "login" (passed)
-     
-def test_login_form_should_be_in_page(browser):   
-    link = "http://selenium1py.pythonanywhere.com"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_form()    # проверяем содержит ли страница форму логина (failed)
     
-def test_registration_form_should_be_in_page(browser):   
-    link = "http://selenium1py.pythonanywhere.com"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_register_form() # проверяем содержит ли страница форму регистрации (failed)      
