cf7bb1e1bed5e9432f95c6cc02a2a26761e647ee
roman romanov <romanoff280@yandex.ru>
2022-07-01 15:57:53 +0500

    update product_page

--- pages/product_page.py
+++ pages/product_page.py
@@ -13,10 +13,14 @@ class ProductPage(BasePage):
         # проверка одинаковы ли цены на товар в карточке товара и в сообщении с добавлением товара.
         total_basket = self.browser.find_element(*ProductPageLocators.MESSAGE_PRICE)
         product_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE)
+        print(total_basket.text)
+        print(product_price.text)
         assert total_basket.text == product_price.text, "price not equal"
 
     def check_equal_names(self):
         # проверка одинаковы ли названия товара в карточке и в сообщении с добавлением товара     
         name_book = self.browser.find_element(*ProductPageLocators.BOOK_NAME)
         name_in_message = self.browser.find_element(*ProductPageLocators.BOOK_NAME_IN_MESSAGE)
+        print(name_book.text)
+        print(name_in_message.text)
         assert name_book.text == name_in_message.text, "names not equal"
